51fbb2315e20be926d39088698353c1a528af375
Nicklas Thor <nicke.tor.86@gmail.com>
2024-02-09 10:54:19 +0100

    Update Program.cs to loop creaing products and to add created products to a list called listProducts. - Update Program.cs to loop to allow user to create many products until user quits making new products. - Update Program.cs to add user created products to a list called listProducts. - Added test code to show content of listProducts.

--- week6_mini_projekt/Program.cs
+++ week6_mini_projekt/Program.cs
@@ -1,8 +1,31 @@
-﻿using week6_mini_projekt;
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+using week6_mini_projekt;
 
-// set newProduct to a new Product based on user input or null if user quit the process of creating new Product
-Product newProduct = Utils.UserCreateProduct();
+//
+List<Product> listProducts = [];
 
-if (newProduct != null) { Display.DisplayColorMsg("THE END newProduct EXIST", "yellow"); }
-    
-else { Display.DisplayColorMsg("THE END DONT newProduct EXIST", "yellow"); }
+// While loop for user to make new products until user quit making products.
+while (true)
+{
+    // set newProduct to a new Product based on user input or null if user quit the process of creating new Product
+    Product newProduct = Utils.UserCreateProduct();
+
+    // if user as created a new Product added it to listProducts
+    if (newProduct != null) 
+    { 
+        Display.DisplayColorMsg("THE END newProduct EXIST SO ADDING IT TO LIST", "yellow");
+        listProducts.Add(newProduct);
+    }
+    // else user has quit making new product then break while loop for making new products.
+    else 
+    { 
+        Display.DisplayColorMsg("THE END DONT newProduct EXIST SO BREAKING WHILE LOOP", "yellow");
+        break;
+    }
+}
+
+foreach (Product product in listProducts)
+{
+    Display.DisplayColorMsg("product: " + product.Category + product.Name + product.Price, "yellow");
+}
