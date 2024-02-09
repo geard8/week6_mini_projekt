1d205198718ec766a351bb80a59b6d19df6e6ab4
Nicklas Thor <nicke.tor.86@gmail.com>
2024-02-09 13:46:50 +0100

    Update Program.cs and Utils.cs by moving loop for creating products to new method UserCreateProducts and using it instead.
    - Update Program.cs by removeing loop for creating products and replaceing it with call of method UserCreateProducts to handle it.
    -Update Utils.cs by adding method UserCreateProducts to that allow user to create products until quits making products.

--- week6_mini_projekt/Program.cs
+++ week6_mini_projekt/Program.cs
@@ -1,22 +1,7 @@
 ﻿using week6_mini_projekt;
 
-// While loop for user to make new products until user quit making products.
-while (true)
-{
-    // set newProduct to a new Product based on user input or null if user quit the process of creating new Product
-    Product newProduct = Utils.UserCreateProduct();
-
-    // if user as created a new Product added it to list Products. Products is a static list from class Product.
-    if (newProduct != null) 
-    { 
-        Product.Products.Add(newProduct);
-    }
-    // else user has quit making new product then break while loop for making new products.
-    else 
-    { 
-        break;
-    }
-}
+// User create products that are added to a list Products that is a static list from class Product.
+Utils.UserCreateProducts();
 
 // Show all products to user in a sorted way(low to high price) and total sum of all products price.
 Product.ShowProducts();
