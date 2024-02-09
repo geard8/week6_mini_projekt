5f5531c2c56bc7332254a137969c0d0018b448b8
Nicklas Thor <nicke.tor.86@gmail.com>
2024-02-09 14:50:07 +0100

    Update Program.cs by adding loop for user to chose what to do next.
    - Add while loop after user is done creating product show list of products that ask user fir what to do next. This is done by giving them the option to choice to create more products, search for products or quiting.
    - Add that the program end when user chose to quit program.
    - Add so user can create more product when user chose to do so.
    - Add where the code that handle search for product when user want to search for products can be run.
    - TODO add code for handling search for products and make it run when user want to search for products.

--- week6_mini_projekt/Program.cs
+++ week6_mini_projekt/Program.cs
@@ -5,3 +5,46 @@ Utils.UserCreateProducts();
 
 // Show all products to user in a sorted way(low to high price) and total sum of all products price.
 Product.ShowProducts();
+
+ConsoleKeyInfo cki; // used to store key pressed by the user
+
+// Give user option to make more products, search for a product or quit program by key input.
+while (true)
+{
+    // Ask user and get key input from user to chose what to do next.
+    while (true)
+    {
+        Console.WriteLine("Continue creating products - press \"P\" | search product - press \"S\" | To quit - press \"Q\"");
+        cki = Console.ReadKey();
+        Console.WriteLine();
+        if (cki.Key == ConsoleKey.P || cki.Key == ConsoleKey.S || cki.Key == ConsoleKey.Q)
+        {
+            break;
+        }
+        else
+        {
+            Display.DisplayColorMsg("Not valid key, valid key are P, S or Q", "red");
+        }
+    }
+
+    // If cki is P then user can create more products and after show list of all products
+    if (cki.Key == ConsoleKey.P)
+    {
+        Utils.UserCreateProducts();
+        Product.ShowProducts();
+    }
+    // if cki is S then search for products based on name.
+    else if (cki.Key == ConsoleKey.S)
+    {
+        // TODO search for products based on name
+    }
+    else if (cki.Key == ConsoleKey.Q)
+    {
+        Display.DisplayColorMsg("Quiting program, hope to see you soon", "green");
+        break;
+    }
+    else { 
+        Display.DisplayColorMsg("ERROR: cki.Key it not P, S or Q yet have someway passed previus validation check to make sure it is.", "yellow");
+        break;
+    }
+}
